bb4f2249d60ae1eb2f0596d5d41b6f692d1f1b66
cuihongkai <hongkaicui@zlshanglv.com>
2017-07-04 17:34:50 +0800

    Initial Commit

---
+++ AddressBook-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddressBook-Swift
+//
+//  Created by 尊旅环球游 on 2017/7/4.
+//  Copyright © 2017年 chk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AddressBook-SwiftTests/AddressBook_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  AddressBook_SwiftTests.swift
+//  AddressBook-SwiftTests
+//
+//  Created by 尊旅环球游 on 2017/7/4.
+//  Copyright © 2017年 chk. All rights reserved.
+//
+
+import XCTest
+@testable import AddressBook_Swift
+
+class AddressBook_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
